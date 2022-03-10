21fe7580167581e6b7c7b2f803dcbacbd048a2c8
MRossa157 <maxim.rossa@mail.ru>
2022-03-10 17:35:56 +0300

    added fibonachi function v1.0

--- nigger.cpp
+++ nigger.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
+
+int fib(int n) {
+    if (n == 1 or n == 2) {
+        return 1;
+    }
+    else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
 int main()
 {
-    cout << "Hello world!";
+    cout << "Hello world!" << endl;
 }
 
